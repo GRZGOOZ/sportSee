72f1b566c4834d6264b7ef4f562d51202e0370d9
Grégoire de Fleurac <140061637+GRZGOOZ@users.noreply.github.com>
2023-10-11 10:03:31 +0200

    piechart score center fix

--- src/pages/Dashboard/components/Score/Score.js
+++ src/pages/Dashboard/components/Score/Score.js
@@ -1,4 +1,4 @@
-import { PieChart, Pie, Cell} from "recharts";
+import { PieChart, Pie, Cell, Text } from "recharts"; // Import Text from recharts
 import styles from "./Score.module.scss";
 
 const Score = ({ userData }) => {
@@ -13,35 +13,38 @@ const Score = ({ userData }) => {
     { name: "Remaining", value: 100 - percentage },
   ];
 
-  const COLORS = [{ fill: "#E60000" }, { fill: "#FAFAFA" }];
+  const COLORS = ["#E60000", "#FAFAFA"]; // Use an array of color strings
 
   const startAngle = 180;
 
   return (
     <div className={styles.score}>
       <h4 className={styles.score_title}>Score</h4>
-        <PieChart width={200} height={200}>
-          <Pie
-            data={data}
-            dataKey="value"
-            startAngle={startAngle}
-            endAngle={startAngle - 360}
-            innerRadius="70%"
-            outerRadius="80%"
-            cornerRadius={20}
-            stroke="none"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index].fill} />
-            ))}
-          </Pie>
-        </PieChart>
-      <p className={styles.score_percentage}>
+      <PieChart width={200} height={200}>
+        <Pie
+          data={data}
+          dataKey="value"
+          startAngle={startAngle}
+          endAngle={startAngle - 360}
+          innerRadius="70%"
+          outerRadius="80%"
+          cornerRadius={20}
+          stroke="none"
+        >
+          {data.map((entry, index) => {
+            return <Cell key={`cell-${index}`} fill={COLORS[index]} />;
+          })}
+          <Text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle">
+            {percentage.toFixed(0)}%
+          </Text>
+        </Pie>
+      </PieChart>
+      <div className={styles.score_percentage}>
         <span className={styles.score_percentage_res}>
           {percentage.toFixed(0)}%
         </span>
         <span className={styles.score_percentage_txt}>de votre objectif</span>
-      </p>
+      </div>
     </div>
   );
 };
